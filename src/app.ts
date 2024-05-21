b034636086b4011491d4da19b4abcbde551d1606
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-21 12:24:49 +0600

    first_commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import apiRouterV1 from "./routers/__root_v1";
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Help"
+    });
+})
+
+app.use('/api/v1', apiRouterV1);
+
+
+export default app;
---
+++ src/routers/__root_v1.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const apiRouterV1 = express.Router()
+
+
+export default apiRouterV1;
