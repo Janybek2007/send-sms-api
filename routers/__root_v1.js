ed922d3b079c88eb98badafa9f16a9e91965a838
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-21 15:59:38 +0600

    update .env

--- routers/__root_v1.js
+++ routers/__root_v1.js
@@ -1,8 +1,13 @@
 import express from 'express'
-import userRouters from "./v1/user-routers.js";
+import userController from "../controllers/user-controller.js";
+import authMiddleware from "../middlewares/auth-middleware.js";
 
 const apiRouterV1 = express.Router()
 
-apiRouterV1.use(userRouters)
+apiRouterV1.post("/user/create", userController.createUser);
+apiRouterV1.get("/user/get/current", authMiddleware, userController.getCurrentUser);
+apiRouterV1.get("/user/get/all", userController.getAllUser);
+apiRouterV1.get("/user/generate-code", userController.generateCode);
+
 
 export default apiRouterV1;
