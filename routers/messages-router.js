2569e934353247804ca263c5c4fc4077c24e6d57
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-23 15:15:27 +0600

    add authMiddleware all messages router/endpoints

--- routers/messages-router.js
+++ routers/messages-router.js
@@ -3,17 +3,20 @@ import authMiddleware from '../middlewares/auth-middleware.js'
 import messagesController from '../controllers/messages-controller.js'
 const router = Router()
 
-router.get('/messages/get/:messagesId', messagesController.getMessages)
+router.get('/messages/get/:messagesId',authMiddleware, messagesController.getMessages)
 router.post(
 	'/messages/:messagesId/send/message',
+	authMiddleware,
 	messagesController.sendMessage
 )
 router.delete(
 	'/messages/:messagesId/delete/message/:messageId',
+	authMiddleware,
 	messagesController.deleteMessage
 )
 router.patch(
 	'/messages/:messagesId/update/message/:messageId',
+	authMiddleware,
 	messagesController.updateMessage
 )
 
