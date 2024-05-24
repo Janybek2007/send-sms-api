2e69713fd7aead8cf75fe45bb18ea118783bdb27
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-24 12:53:33 +0600

    add corst to socket.io

--- websocket-server.js
+++ websocket-server.js
@@ -1,14 +1,19 @@
 // /websocket-server.js
-import { Server } from 'socket.io';
+import { Server } from 'socket.io'
 
 export default function websocketServer(httpServer) {
-    const io = new Server(httpServer);
+	const io = new Server(httpServer, {
+		cors: {
+			origin: '*',
+			methods: ['POST', 'GET', 'PATCH', 'DELETE']
+		}
+	})
 
-    io.on('connection', (socket) => {
-        console.log('WebSocket Client Connected');
+	io.on('connection', socket => {
+		console.log('WebSocket Client Connected')
 
-        socket.on('disconnect', () => {
-            console.log('WebSocket Client Disconnected');
-        });
-    });
+		socket.on('disconnect', () => {
+			console.log('WebSocket Client Disconnected')
+		})
+	})
 }
