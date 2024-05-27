566babf0e81156db57aa2a90c0f3ffa5098a042d
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-27 13:26:19 +0600

    rename app

--- services/send_sms-services.js
+++ services/send_sms-services.js
@@ -1,29 +1,34 @@
 class SendSMSServices {
-    async send(text, to) {
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                "Authorization": `App ${process.env.API_KEY}`,
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                "messages": [{
-                    "destinations": [{"to": to}],
-                    "from": "Slash",
-                    "text": text
-                }]
-            })
-        };
+	async send(text, to) {
+		const requestOptions = {
+			method: 'POST',
+			headers: {
+				Authorization: `App ${process.env.API_KEY}`,
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({
+				messages: [
+					{
+						destinations: [{ to: to }],
+						from: 'Discussify',
+						text: text
+					}
+				]
+			})
+		}
 
-        try {
-            const response = await fetch(`https://${process.env.API_URL}/sms/2/text/advanced`, requestOptions);
-            const result = await response.text();
-            return result
-        } catch (error) {
-            console.error(error);
-        }
-    }
+		try {
+			const response = await fetch(
+				`https://${process.env.API_URL}/sms/2/text/advanced`,
+				requestOptions
+			)
+			const result = await response.text()
+			return result
+		} catch (error) {
+			console.error(error)
+		}
+	}
 }
 
-export default new SendSMSServices();
+export default new SendSMSServices()
